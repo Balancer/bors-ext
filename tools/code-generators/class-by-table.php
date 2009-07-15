30b6cb1a5d73bc9857c637d1768a3bce54a01a0a
Balancer <balancer@balancer.ru>
2009-07-15 04:26:54 +0400

    +++

--- tools/code-generators/class-by-table.php
+++ tools/code-generators/class-by-table.php
@@ -7,12 +7,12 @@ main($argv);
 
 function main($argv)
 {
-	if(empty($argv[1]))
+	if(empty($argv[2]))
 		exit("Use {$argv[0]} table [db] [host]\n");
 
-	$table = $argv[1];
-	$db    = empty($argv[2]) ? config('main_bors_db') : $argv[2];
-//	$host  = empty($argv[3]) ? 'localhost' : $argv[3];
+	$table = $argv[2];
+	$db    = empty($argv[3]) ? config('main_bors_db') : $argv[3];
+//	$host  = empty($argv[4]) ? 'localhost' : $argv[4];
 
 	$dbh = new driver_mysql($db);
 
