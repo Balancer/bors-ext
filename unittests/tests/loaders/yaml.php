59314d9c8eb99b902d3876cd123447e6edc1bcff
Balancer <balancer@balancer.ru>
2011-07-21 19:08:39 +0400

    Ещё один тест.

--- unittests/tests/loaders/yaml.php
+++ unittests/tests/loaders/yaml.php
@@ -20,5 +20,21 @@ class loaders_yaml_unittest extends PHPUnit_Framework_TestCase
 		$x = bors_find_first($cls, array('text' => 'Текст'));
 		$this->assertNotNull($x);
 		$this->assertEquals('Заголовок', $x->title());
+
+		$db->query('DROP TABLE IF EXISTS yaml_test2;');
+
+		$cls = 'bors_tests_yaml_test2';
+
+		$x = bors_new($cls, array(
+			'title' => 'Заголовок2',
+			'text' => 'Текст2',
+		));
+
+		$this->assertNotNull($x);
+		$this->assertEquals(1, $x->id());
+
+		$x = bors_find_first($cls, array('text' => 'Текст2'));
+		$this->assertNotNull($x);
+		$this->assertEquals('Заголовок2', $x->title());
 	}
 }
