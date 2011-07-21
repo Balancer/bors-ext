34aec03f78c73219011b4a137539514c88824028
Balancer <balancer@balancer.ru>
2011-07-21 18:28:47 +0400

    Зачатки автогенерации классов из YAML. Как ни странно, навскидку вполне работает.

---
+++ unittests/tests/loaders/yaml.php
@@ -0,0 +1,24 @@
+<?php
+
+class loaders_yaml_unittest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+		$db = new driver_mysql(config('unit-test.mysql.db'));
+		$db->query('DROP TABLE IF EXISTS bors_tests_yaml_test1s;');
+
+		$cls = 'bors_tests_yaml_test1';
+
+		$x = bors_new($cls, array(
+			'title' => 'Заголовок',
+			'text' => 'Текст',
+		));
+
+		$this->assertNotNull($x);
+		$this->assertEquals(1, $x->id());
+
+		$x = bors_find_first($cls, array('text' => 'Текст'));
+		$this->assertNotNull($x);
+		$this->assertEquals('Заголовок', $x->title());
+	}
+}
