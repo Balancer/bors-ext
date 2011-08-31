0bb5ba43798b41a043a721468103c4d32560fa54
Balancer <balancer@balancer.ru>
2011-08-31 21:14:53 +0400

    ++

--- cli/init.php
+++ cli/init.php
@@ -3,5 +3,11 @@
 if(file_exists('setup_php'))
 	include_once('setup.php');
 
+if(!defined('BORS_CORE'))
+{
+	define('BORS_HOST', dirname(dirname(__FILE__)));
+	define('BORS_CORE', dirname(BORS_HOST).'/bors-core');
+}
+
 include_once(BORS_CORE.'/init.php');
 config_set('system.use_sessions', false);
