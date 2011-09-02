36f38e8649f6741415021257e69a4200b681207e
Balancer <balancer@balancer.ru>
2011-09-02 16:41:47 +0400

    Так лучше.

--- classes/bors/module/multicomments.php
+++ classes/bors/module/multicomments.php
@@ -14,4 +14,21 @@ class bors_module_multicomments extends bors_module
 
 		return parent::pre_show();
 	}
+
+	function body_data()
+	{
+		$data = array();
+		if($uid = $this->arg('uid'))
+		{
+			$data['uid'] = $uid;
+			$data['pid'] = md5($uid);
+		}
+		else
+		{
+			$data['uid'] = preg_replace('!^(http://[^/]+):\d+!', '$1', bors()->main_object()->url(1));
+			$data['pid'] = md5($data['uid']);
+		}
+
+		return array_merge(parent::body_data(), $data);
+	}
 }
--- demo-make/make.sh
+++ demo-make/make.sh
@@ -10,7 +10,7 @@ rm bors-demo -rf
 mkdir bors-demo
 hg init bors-demo
 
-hg convert bors-core bors-demo --filemap $EXT/make/bors-demo.core.map
+hg convert bors-core bors-demo --filemap $EXT/demo-make/bors-demo.core.map
 
 pushd bors-demo
 hg up
@@ -21,7 +21,7 @@ for i in ext third-party tutorial; do
 	mkdir ../bors-demo-$i
 	hg init ../bors-demo-$i
 
-	hg convert ../bors-$i ../bors-demo-$i --filemap $EXT/make/bors-demo.$i.map
+	hg convert ../bors-$i ../bors-demo-$i --filemap $EXT/demo-make/bors-demo.$i.map
 
 	echo === Pull bors-demo-$i
 	hg pull -f -u ../bors-demo-$i
@@ -29,17 +29,17 @@ for i in ext third-party tutorial; do
 	hg merge
 	echo ... commit
 	hg ci -m Merge
-#	rm ../bors-demo-$i -rf
+	rm ../bors-demo-$i -rf
 done
 
 popd
 
-cp $EXT/make/setup.php.demo bors-demo/cli/setup.php
-cp -r $EXT/make/jquery-local bors-demo/webserver/htdocs
+cp $EXT/demo-make/setup.php.demo bors-demo/cli/setup.php
+cp -r $EXT/demo-make/jquery-local bors-demo/webserver/htdocs
 
 # echo -e "<?php\nrequire_once 'config-3rd.php';" >> bors-demo/ext/cli/config.php
 
-cat ext.config.inc.demo >> bors-demo/ext/config.php
+cat $EXT/demo-make/ext.config.inc.demo >> bors-demo/ext/config.php
 
 cd bors-demo/webserver
 clear
