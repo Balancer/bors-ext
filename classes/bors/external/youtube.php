48b07d88805f17318b87bd294006b46090352a57
Balancer <balancer@balancer.ru>
2012-01-20 17:48:00 +0400

    fix

--- classes/bors/external/youtube.php
+++ classes/bors/external/youtube.php
@@ -16,6 +16,6 @@ class bors_external_youtube extends bors_object
 	static function url2html($url)
 	{
 		$id = self::id_prepare($url);
-		return bors_lcml_tags_pairs_youtube::html($id);
+		return bors_lcml_tag_pair_youtube::html($id);
 	}
 }
