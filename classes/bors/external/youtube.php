60e8f295f6db93aec8aa5e91a13cbd78561e02d1
Balancer <balancer@balancer.ru>
2012-01-23 23:06:10 +0400

    Fix.

--- classes/bors/external/youtube.php
+++ classes/bors/external/youtube.php
@@ -26,6 +26,7 @@ class bors_external_youtube extends bors_object
 		// http://www.youtube.com/watch?v=X76LmiHVFsM&feature=player_embedded
 		// http://www.youtube.com/watch?v=TXxcR3qgyYQ&playnext=1&list=PL21AA194D7FBBA2D9
 		// https://www.youtube.com/watch?v=21El16OPZoc
+		// http://www.youtube.com/watch?feature=player_embedded&v=zZPNaMDD-A8
 		$text = preg_replace('!^\s*(https?://[^/]+youtube\.\w+/watch\S+)\s*$!mie', "bors_external_youtube::url2bb('$1');", $text);
 		return $text;
 	}
@@ -33,6 +34,8 @@ class bors_external_youtube extends bors_object
 	// Трансляция ссылки на YouTube ролик в [youtube]<video-id>[/youtube]
 	static function url2bb($url)
 	{
+		$url = bors_entity_decode($url);
+//		echo $url."\n";
 		bors_function_include('url/parse');
 		$video_id = bors_url_parse($url, 'query', 'v');
 		return "[youtube]{$video_id}[/youtube]";
