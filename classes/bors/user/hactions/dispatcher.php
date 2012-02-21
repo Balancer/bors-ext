c4b3d8d5b0be5a9a971a988b1d78f6f8d1c77f12
Balancer <balancer@balancer.ru>
2012-02-21 07:57:56 +0400

    Исправления hactions.

--- classes/bors/user/hactions/dispatcher.php
+++ classes/bors/user/hactions/dispatcher.php
@@ -9,23 +9,27 @@ class bors_user_hactions_dispatcher extends base_object
 		if(!$haction)
 			return bors_message(ec('Извините, но выбранное Вами действие невозможно. Неверная, уже использованная или устаревшая ссылка.'));
 
+		// Криво, но нам нужен оригинальный объект, так что перезагружаем уже с нужным:
+		$haction = bors_find_first($haction->haction_class_name(), array('id' => $this->id()));
+
 		if($haction->expire_time() < time())
 			return bors_message(ec('Извините, выбранное Вами действие невозможно. Устаревшая ссылка.'));
 
 		$actor = bors_load($haction->actor_class_name(), $haction->actor_target_id());
 
 		$haction->set_attr('need_save', false);
+
 		if($method = $haction->actor_method())
-		{
-			if($actor->$method(json_decode($haction->actor_attributes(), true), $haction) === true)
-				$ret = true;
-		}
+			$ret = $actor->$method(json_decode($haction->actor_attributes(), true), $haction);
 		else
-			$ret = go($actor->url());
+			$ret = $actor->url();
 
 		if(!$haction->attr('need_save'))
 			$haction->clean();
 
-		return $ret;
+		if($ret === true)
+			return true;
+
+		return go($ret);
 	}
 }
