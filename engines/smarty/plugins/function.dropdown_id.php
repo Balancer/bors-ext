4f5a6f7c068ff65b313ab3374c6e291286823897
Balancer <balancer@balancer.ru>
2012-02-27 02:53:19 +0400

    Пробуем новое поле форм, совмещающее ввод ID и выбор объекта. Плюс для юниттестов теперь не обязательно переопределять cache_dir.

---
+++ engines/smarty/plugins/function.dropdown_id.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once('inc/bors/lists.php');
+
+function smarty_function_dropdown_id($params, &$smarty)
+{
+	extract($params);
+
+	$params['name'] = '_'.$name;
+
+	$form = $smarty->get_template_vars('form');
+	bors_form::append_attr('override_fields', $name);
+
+	$html = "<table class=\"null\"><tr><td>ID:</td><td>";
+	$html .= bors_forms_input::html($params);
+	$html .= "</td><td>";
+
+	template_jquery();
+	template_js("\$(function() {
+	\$('select[name={$name}]').change(function(){
+		\$('input[name=_{$name}]').val(\$(this).val())
+	});
+});");
+
+	unset($params['maxlength'], $params['size']);
+
+	$params['name'] = $name;
+	$html .= bors_forms_dropdown::html($params);
+	$html .= "</td></tr></table>";
+
+	echo $html;
+}
