cf8c1815ebf0d5dcf4b0381a2ea33b72f1926ef8
Balancer <balancer@balancer.ru>
2012-04-03 15:02:30 +0400

    Тэг [mp3]...[/mp3] для флеш-плеера.

---
+++ classes/lcml/tag/pair/mp3.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+	Вставка MP3-ссылки с flash-проигрывателем
+	Пример использования: [mp3]http://airbase.ru/events/2012/0403-fire-moscow-city/skyscraper_00m_00s__20m_02s_40h.mp3[/mp3]
+	Ссылка с примером: http://balancer.ru/g/p2770643
+*/
+
+class lcml_tag_pair_mp3 extends bors_lcml_tag_pair
+{
+	function html($mp3, &$params = array())
+	{
+		// via http://www.labnol.org/internet/design/html-embed-mp3-songs-podcasts-music-in-blogs-websites/2232/
+		// Пример использования: http://balancer.ru/g/p2770643
+		return "<embed type=\"application/x-shockwave-flash\" flashvars=\"audioUrl=$mp3\" "
+			."src=\"http://www.google.com/reader/ui/3523697345-audio-player.swf\" width=\"640\" height=\"27\" quality=\"best\">"
+			."</embed><br/>"
+			."<a href=\"$mp3\" class=\"transgray\">Скачать: ".basename($mp3)."</a>";
+	}
+}
