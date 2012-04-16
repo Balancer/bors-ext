ba4234333cfe2c3feefcdb3a5acc1db0bb94ed44
Balancer <balancer@balancer.ru>
2012-04-16 13:19:31 +0400

    Модули.

--- classes/bors/views/main.php
+++ classes/bors/views/main.php
@@ -2,13 +2,17 @@
 
 class bors_views_main extends bors_paginated
 {
-	function title() { return bors_lib_object::get_foo($this->main_class(), 'class_title_m'); }
+	function _title_def() { return bors_lib_object::get_foo($this->main_class(), 'class_title_m'); }
 	function nav_name() { return bors_lib_object::get_foo($this->main_class(), 'class_title_m'); }
 
-	function main_class()
+	function _main_class_def()
 	{
 		$class_name = str_replace('_views_main', '', $this->class_name());
 		$class_name = str_replace('_main', '', $class_name);
 		return bors_unplural($class_name);
 	}
+
+	function _project_name_def() { return bors_core_object_defaults::project_name($this); }
+	function _section_name_def() { return bors_core_object_defaults::section_name($this); }
+	function _config_class_def() { return bors_core_object_defaults::config_class($this); }
 }
--- classes/bors/views/modules/cards.php
+++ classes/bors/views/modules/cards.php
@@ -18,12 +18,32 @@ class bors_views_modules_cards extends bors_module
 	function body_data()
 	{
 		$where = $this->arg('where', array());
-		$where['order'] = $this->arg('order', defval($where, 'order', 'title'));
+		$where['order'] = $this->arg('order', defval($where, 'order', '-create_time'));
 		$where['limit'] = $this->arg('limit', defval($where, 'limit', 10));
+
+		if($this->args('owned_only'))
+			$where['owner_id'] = bors()->user_id();
+
 		$items = bors_find_all($this->item_class(), $where);
 		return array_merge(parent::body_data(), array(
 			$this->items_name() => $items,
 			'items' => $items,
 		));
+	}
+
+	function new_titled_link()
+	{
+		$class_name = $this->item_class();
+		$foo = new $class_name(NULL);
+
+		$x = array(
+			'f' => ec('ая'),
+			'm' => ec('ый'),
+			'n' => ec('ое'),
+		);
+
+		$title = ec('Нов').$x[$foo->class_title_gender()].' '.bors_lower($foo->class_title());
+
+		return "<a href=\"{$foo->admin()->urls('new')}\">$title</a>";
 	}
 }
