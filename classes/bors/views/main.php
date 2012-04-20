bd6338397356b8903e5b3f0cc01911bd85294945
Balancer <balancer@balancer.ru>
2012-04-21 02:41:00 +0400

    Что-то было.

--- classes/bors/views/main.php
+++ classes/bors/views/main.php
@@ -15,4 +15,13 @@ class bors_views_main extends bors_paginated
 	function _project_name_def() { return bors_core_object_defaults::project_name($this); }
 	function _section_name_def() { return bors_core_object_defaults::section_name($this); }
 	function _config_class_def() { return bors_core_object_defaults::config_class($this); }
+
+	function search_form()
+	{
+		$search = bors_load_ex('bors_auto_search', NULL, array(
+			'class' => $this->section_name(),
+		));
+
+		return $search->body().'<br/>';
+	}
 }
