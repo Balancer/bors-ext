f6b54496185fbe0b02c6450a122b9b7cf8e3d74b
Balancer <balancer@balancer.ru>
2012-07-06 03:52:03 +0400

    ++

---
+++ classes/bors/htdocs/index.php
@@ -0,0 +1,24 @@
+<?php
+
+class bors_htdocs_index extends bors_page
+{
+	function title() { return ec('Содержимое каталога ').$this->arg('path'); }
+
+	function body_data()
+	{
+		$dirs = array();
+		$files = array();
+		$path = $this->arg('path');
+
+		foreach(glob(bors()->server()->root().$path.'*') as $f)
+			if(is_dir($f))
+				$dirs[] = array('url' => $path.basename($f).'/', 'base' => basename($f));
+			else
+				$files[] = array('url' => $path.basename($f), 'base' => basename($f));
+
+		usort($dirs,  function($x, $y) { return strcmp($x['base'], $y['base']);});
+		usort($files, function($x, $y) { return strcmp($x['base'], $y['base']);});
+
+		return compact('dirs', 'files') + parent::body_data();
+	}
+}
