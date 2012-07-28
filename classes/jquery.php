78442b0db016a95d223ba65ec6e297e7ba68007c
Balancer <balancer@balancer.ru>
2012-07-28 21:47:35 +0400

    lightBox.

--- classes/jquery.php
+++ classes/jquery.php
@@ -21,4 +21,26 @@ class jquery
 	{
 		template_jquery_document_ready($js_code);
 	}
+
+	static function use_html()
+	{
+		$link = '/_bors3rdp/jquery/jquery-'.config('jquery.version').'.min.js';
+
+		return "<!--[[use js=\"$link\"]]-->";
+	}
+
+	static function on_ready_html($js_code)
+	{
+		static $have = array();
+		$hash = md5($js_code);
+
+		if(!empty($have[$hash]))
+			return '<!--already-->';
+
+//		$have[$hash] = true;
+
+		$html = ""; // jquery::use_html();
+		$html .= "<script type=\"text/javascript\" rel=\"test123\"><!--\n\$(document).ready(function() {\n{$js_code}\n});\n--></script>\n";
+		return $html.'<!--new123-->';
+	}
 }
