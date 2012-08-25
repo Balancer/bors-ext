486a21a2cf9a5d37593258ce09f84dfc6564d022
Balancer <balancer@balancer.ru>
2012-08-25 23:32:56 +0400

    Возможность задать имя mp3 явно.

--- classes/lcml/tag/pair/mp3.php
+++ classes/lcml/tag/pair/mp3.php
@@ -10,11 +10,12 @@ class lcml_tag_pair_mp3 extends bors_lcml_tag_pair
 {
 	function html($mp3, &$params = array())
 	{
+		$title = defval($params, 'title', basename($mp3));
 		// via http://www.labnol.org/internet/design/html-embed-mp3-songs-podcasts-music-in-blogs-websites/2232/
 		// Пример использования: http://balancer.ru/g/p2770643
 		return "<embed type=\"application/x-shockwave-flash\" flashvars=\"audioUrl=$mp3\" "
 			."src=\"http://www.google.com/reader/ui/3523697345-audio-player.swf\" width=\"640\" height=\"27\" quality=\"best\">"
 			."</embed><br/>"
-			."<a href=\"$mp3\" class=\"transgray\">Скачать: ".basename($mp3)."</a>";
+			."<a href=\"$mp3\" class=\"transgray\">Скачать: {$title}</a>";
 	}
 }
