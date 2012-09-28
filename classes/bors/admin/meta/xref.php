154accac9ce2d477ae2cb79f7bfbb1818a3c3a30
Balancer <balancer@balancer.ru>
2012-09-28 04:09:29 +0400

    Это у нас, всё же, xref.

--- classes/bors/admin/meta/xref.php
+++ classes/bors/admin/meta/xref.php
@@ -5,7 +5,7 @@
 	Пример: http://admin.aviaport.wrk.ru/directory/airlines/266/airports/
 */
 
-class bors_admin_meta_joined extends bors_admin_page
+class bors_admin_meta_xref extends bors_admin_page
 {
 //	function joined() { return bors_find_all($this->joined_class(), array(''));
 }
