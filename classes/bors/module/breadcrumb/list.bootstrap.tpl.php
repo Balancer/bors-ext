8219bad22958a1c20abba32a870f498236c6931f
Balancer <balancer@balancer.ru>
2012-10-01 01:12:27 +0400

    «Хлебные крошки» на Twitter Bootstrap

---
+++ classes/bors/module/breadcrumb/list.bootstrap.tpl.php
@@ -0,0 +1,22 @@
+<ul class="breadcrumb">
+<?php
+
+foreach($links as $linkline)
+{
+	$first = true;
+	foreach($linkline as $obj)
+	{
+		if($first)
+			$first = false;
+		else
+			echo " <span class=\"divider\">/</span>";
+
+		echo "<a href=\"{$obj->url(1)}\" title=\"".htmlspecialchars($obj->title())."\"";
+		if($nav_obj->url(1) == $obj->url(1))
+			echo " class=\"active\"";
+		echo '>'.htmlspecialchars($obj->nav_name()).'</a>';
+	}
+	echo "<br />";
+}
+?>
+</ul>
