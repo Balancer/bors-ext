89feeaf4a3df03aa4048c7852e0a6912a3113cdc
Balancer <balancer@balancer.ru>
2012-10-28 21:05:21 +0400

    Bootstrap имеет смысл грузить первым.

--- classes/twitter/bootstrap.php
+++ classes/twitter/bootstrap.php
@@ -4,8 +4,8 @@ class twitter_bootstrap
 {
 	function load()
 	{
-		bors_use('/_bors3rdp/'.config('bootstrap.path').'/css/bootstrap.min.css');
-		bors_use('/_bors3rdp/'.config('bootstrap.path').'/css/bootstrap-responsive.min.css');
+		bors_use('pre:/_bors3rdp/'.config('bootstrap.path').'/css/bootstrap.min.css');
+		bors_use('pre:/_bors3rdp/'.config('bootstrap.path').'/css/bootstrap-responsive.min.css');
 		jquery::load();
 		bors_use('/_bors3rdp/'.config('bootstrap.path').'/js/bootstrap.min.js');
 	}
