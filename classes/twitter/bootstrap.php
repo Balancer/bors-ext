344c8d7834b0ae00efea7d019fa07df446540500
Balancer <balancer@balancer.ru>
2012-11-01 03:59:46 +0400

    Обновление Bootstrap.

--- classes/twitter/bootstrap.php
+++ classes/twitter/bootstrap.php
@@ -4,8 +4,9 @@ class twitter_bootstrap
 {
 	function load()
 	{
-		bors_use('pre:/_bors3rdp/'.config('bootstrap.path').'/css/bootstrap.min.css');
 		bors_use('pre:/_bors3rdp/'.config('bootstrap.path').'/css/bootstrap-responsive.min.css');
+		bors_use('pre:/_bors3rdp/'.config('bootstrap.path').'/css/bootstrap.min.css');
+
 		jquery::load();
 		bors_use('/_bors3rdp/'.config('bootstrap.path').'/js/bootstrap.min.js');
 		config_set('css_bootstrap_is_loaded', true);
