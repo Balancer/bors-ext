2a047837bf960f9815640b1154c2ff5a40beeacf
Balancer <balancer@balancer.ru>
2012-12-23 09:52:23 +0400

    oops. Слона-то мы и забыли.

--- classes/twitter/bootstrap.php
+++ classes/twitter/bootstrap.php
@@ -11,4 +11,10 @@ class twitter_bootstrap
 		bors_use('/_bors3rdp/'.config('bootstrap.path').'/js/bootstrap.min.js');
 		config_set('css_bootstrap_is_loaded', true);
 	}
+
+	function jquery_ui()
+	{
+		bors_use('pre:/_bors3rdp/bootstrap-plugins/'.config('bootstrap.jquery_ui').'/css/custom-theme/jquery-ui-1.8.16.custom.css');
+		bors_use('/_bors3rdp/bootstrap-plugins/'.config('bootstrap.jquery_ui').'/js/jquery-ui-1.8.16.custom.min.js');
+	}
 }
