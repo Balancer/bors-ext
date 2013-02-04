9fc3ad4972e40af85297e71bd42474b3311e3f7e
Balancer <balancer@balancer.ru>
2013-02-04 04:30:44 +0400

    Давно забытое.

--- classes/bors/page/meta/search.php
+++ classes/bors/page/meta/search.php
@@ -2,10 +2,16 @@
 
 class bors_page_meta_search extends bors_page_meta_main
 {
-	function q() { return (urldecode(@$_GET['q'])); }
+	function title() { return ec('Поиск по ').$this->foo_object()->class_title_dpm(); }
+	function nav_name() { return ec('поиск'); }
+	function is_auto_url_mapped_class() { return true; }
+
+	function main_class() { bors_throw(ec('Не определён класс для поиска')); }
+
+	function q() { return trim(urldecode(@$_GET['q'])); }
 	function qfc()
 	{
-		$fc = urldecode(@$_GET['fc']);
+		$fc = trim(urldecode(@$_GET['fc']));
 
 		if(strlen($fc) == 2)
 			$fc = ec($fc);
@@ -13,14 +19,13 @@ class bors_page_meta_search extends bors_page_meta_main
 		return $fc;
 	}
 
-    function title() { return ec('Результат поиска «').$this->q().ec('»: '); }
-
-    function nav_name() { return ec('«'). $this->q() .ec('»'); }
-
 	function pre_parse()
 	{
 		if(!$this->qfc() && !$this->q())
-			return go('/directory/airports/');
+		{
+			$parents = $this->parents();
+			return go(empty($parents[0]) ? '/' : $parents[0]);
+		}
 
 		return parent::pre_parse();
 	}
@@ -29,40 +34,97 @@ class bors_page_meta_search extends bors_page_meta_main
 	{
 		template_noindex();
 
-		$airports = $this->_airports();
+		$items = $this->items();
 
-		if(sizeof($airports) == 1)
-			return go($airports[0]->url());
+		if(sizeof($items) == 1 && $this->total_items() == 1)
+			return go($items[0]->url());
 
 		return false;
 	}
 
-	function _airports()
-	{
-		if($this->__havec('_airports'))
-			return $this->__lastc();
-
-		$where = array();
-		$where['order'] = 'name_rus';
-
-		$db = new driver_mysql(config('aviaport_db'));
-		if(($q = mysql_real_escape_string($this->q(), $db->link())))
-			$where[] = "(name_rus LIKE '%$q%' OR ICAO_rus LIKE '$q')";
-
-		if(($fc = $this->qfc()))
-			$where['LEFT(name_rus, 1) ='] = $fc;
-
-		return $this->__setc(objects_array('aviaport_directory_airport', $where));
-	}
-
 	function body_data()
 	{
-		$db = new driver_mysql(config('aviaport_db'));
+		$data = parent::body_data();
 
-		return array_merge(parent::body_data(), array(
-			'airports' => $this->_airports(),
-			'letters' => $db->select_array('airports', 'DISTINCT LEFT(name_rus, 1) as fc', array('order' => 'fc')),
-			'q' => $this->q(),
-		));
-    }
+		if(!$this->q() && !$this->qfc())
+			return $data;
+
+		$data['items'] = bors_find_all($this->main_class(), array(
+			'where' => $this->where(),
+			'page' => $this->page(),
+			'per_page' => $this->items_per_page(),
+			'order' => $this->order()));
+
+		$data['query'] = $this->q();
+
+		$main_class = $this->main_class();
+		$foo = new $main_class(NULL);
+
+
+		$fields = $this->get('item_fields');
+		if(!$fields)
+			$fields = bors_lib_object::get_foo($this->main_class(), 'item_list_fields');
+
+		$data['item_fields'] = $fields;
+
+		return $data;
+	}
+
+	function _order_def() { return 'title'; }
+
+
+	function total_items()
+	{
+		return bors_count($this->main_class(), array('where' => $this->where()));
+	}
+
+	function where()
+	{
+		if(!$this->q() && !$this->qfc())
+			return array();
+
+		$q = $qfc = NULL;
+
+		if($this->q())
+			$q = "'%".addslashes($this->q())."%'";
+
+		if($this->qfc())
+			$qfc = "'".addslashes($this->qfc())."%'";
+
+		$qq = array();
+
+		$main_class = $this->main_class();
+		$foo = new $main_class(NULL);
+
+		$properties = $foo->get('searchable_properties', 'title');
+		if(!is_array($properties))
+			$properties = explode(' ', $properties);
+
+		foreach($properties as $p)
+		{
+			if(strpos($p, '`') === false)
+			{
+				$x = bors_lib_orm::parse_property($this->main_class(), $p);
+
+				if(!$x || !$x['name'])
+				{
+					debug_hidden_log('search_properties_error', "Unknown property $p");
+					continue;
+				}
+
+				$field = "`{$x['name']}`";
+			}
+			else
+				$field = $p;
+//			var_dump($this->main_class(), $p, $field);
+			if($q)
+				$qq[] = "$field LIKE {$q}";
+			if($qfc)
+				$qq[] = "$field LIKE {$qfc}";
+		}
+
+		$where = array('('.join(' OR ', $qq).')');
+//		var_dump($where);
+		return $where;
+	}
 }
