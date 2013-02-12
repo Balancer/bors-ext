05b3a31b14ba5cb5ad221e7ca1ee5bdb562da01e
Balancer <balancer@balancer.ru>
2013-02-12 08:07:04 +0400

    ++

--- classes/bors/tools/codegen.php
+++ classes/bors/tools/codegen.php
@@ -2,7 +2,7 @@
 
 class bors_tools_codegen
 {
-	static function class_create($class_type, $class_name, $project_root)
+	static function class_create($class_type, $class_name, $project_root, $table_name = NULL, $db_name = NULL)
 	{
 		var_dump($class_type, $class_name, $project_root);
 		$desc = @self::$types[$class_type];
@@ -19,7 +19,42 @@ class bors_tools_codegen
 		if(file_exists($class_file))
 			bors_throw("Class $class_name already exists in $class_file");
 
-//		file_put_contents($class_file, "extends ");
+		if(!$table_name)
+			$table_name = str_replace(config('project.name').'_', '', $class_name);
+
+		$extends = config('project.name').$extends;
+
+		switch($class_type)
+		{
+			case 'yaml_db':
+				$code = "extends: $extends
+
+config_class: ".config('project.name')."_config
+
+table_name: $table_name
+properties:
+	- id
+	- title
+	- create_time: UNIX_TIMESTAMP(`create_ts`)
+	- modify_time: UNIX_TIMESTAMP(`modify_ts`)
+	- owner_id
+	- last_editor_id
+";
+				break;
+			default:
+				$code = NULL;
+		}
+
+		if(!$code)
+			bors_throw("Can't make code");
+
+		file_put_contents($class_file, $code);
+
+		$foo = bors_foo($class_name);
+		if(!$foo)
+			bors_throw("Can't load created class $class_name");
+
+		$foo->storage()->create_table();
 	}
 
 	static $types = array(
--- classes/bors/util/class.php
+++ classes/bors/util/class.php
@@ -12,29 +12,49 @@ class bors_util_class
 //		var_dump($argv, config('project.name'));
 		@list($class_name, $action, $name, $type) = $argv;
 
+		// Если имя класса из одних букв, то это имя относительно текущего каталога
+		if(preg_match('/^[a-z]+$/', $class_name))
+		{
+			// Поднимаемся по уровням каталогов выше, пока не доберёмся до classes
+			$classes_path = dirname($init_path = getcwd());
+			do
+			{
+				$classes_path = dirname($classes_path);
+			} while($classes_path > '/' && !preg_match('!/classes$!', $classes_path));
+
+			// Если, действительно, нашли каталог /classes/
+			if(preg_match('!/classes$!', $classes_path))
+				$class_name = str_replace('/', '_', str_replace($classes_path.'/', '', $init_path)) . '_' . $class_name;
+		}
+
+
 		if($class_name[0] == '_')
 			$class_name = config('project.name') . $class_name;
 
+		// Если имя класса оканчивается на .php, то это файл и имя берётся из него.
 		if(preg_match('/\.php$/', $class_name))
 			$class_name = bors_util::file_php_to_classname($class_name);
 
 		echo "\twork with $class_name: $action $name(".@$type.")\n";
 
 		if(!preg_match('/^[a-z]\w+[a-z]$/', $class_name))
-		{
-			blib_cli::out("%C%RIncorrect class name: '$class_name'%C%n");
-			break;
-		}
+			return blib_cli::out("%C%RIncorrect class name: '$class_name'%C%n");
 
 		switch($action)
 		{
+			// @list($class_name, $action = 'create', $table_name) = $argv;
+			// bors class <class_name> create [table_name] [type]
 			// bors class _directory_aviation_wtc create
+			// bors class wtc create aviation_wtc
 			case 'create':
-				$type = $name; // Так как в команде на один параметр меньше.
+				$table_name = @$argv[2];	// В команде другой порядок
+				$type = @$argv[3];
 
 				if(!$type)
 					$type = 'yaml_db';
-				bors_tools_codegen::class_create($type, $class_name, BORS_SITE);
+
+				blib_cli::out("%C%GCreate class '$class_name' as $type%C%n");
+				bors_tools_codegen::class_create($type, $class_name, BORS_SITE, $table_name);
 				break;
 
 			// bors class airlines.php add create_time timestamp
