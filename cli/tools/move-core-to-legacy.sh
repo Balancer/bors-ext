905bb6bb3fe5d33634ed2b9678bab926c7625682
Balancer <balancer@balancer.ru>
2013-04-23 23:39:26 +0400

    Чтобы не потерялось.

---
+++ cli/tools/move-core-to-legacy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+TEMP=__repo_temp
+FILEMAP=__filemap
+
+## via http://stackoverflow.com/questions/3643313/mercurial-copying-one-file-and-its-history-to-another-repository
+
+# Create a filemap for the ConvertExtension with the single line:
+# include path/to/file
+
+echo include $1 > $FILEMAP
+
+rm $TEMP  -rf
+mkdir $TEMP
+hg init $TEMP
+
+
+# Then use:
+# hg convert path/to/original path/to/temporary --filemap filemap
+
+hg convert bors-core $TEMP --filemap $FILEMAP
+
+# to create the temporary repository. Next, in the target repository, do:
+# hg pull -f path/to/temporary
+
+cd bors-legacy
+hg pull -uf ../$TEMP
+
+cd ..
+rm -rf $TEMP
+unlink $FILEMAP
