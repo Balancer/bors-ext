5c84382f6e8a6fd80a75cd5f9176505dcbf18e55
Balancer <balancer@balancer.ru>
2013-04-24 01:19:41 +0400

    Проверка на запрещение изображений.

---
+++ htdocs/_bors-ext/js/detect-image-enabled.js
@@ -0,0 +1,53 @@
+// http://stackoverflow.com/questions/8379156/how-to-detect-if-images-are-disabled-in-browser
+detectImageEnabledMode({
+	onDetectImageIsDisabled:function(){
+		top.is_image_disabled = true
+	},
+	onDetectImageIsEnabled:function(){
+		top.is_image_disabled = false
+	}
+});
+
+function detectImageEnabledMode(options){
+	/* define disabled/enabled actions */
+	var actionCounter = 0;
+	var enabledAction = (options.onDetectImageIsEnabled && typeof(options.onDetectImageIsEnabled)=='function')?options.onDetectImageIsEnabled:function(){};
+	var enaledActionRun = function(){
+		if(actionCounter) return;
+		actionCounter++;
+		enabledAction();
+	}
+	var disabledAction = (options.onDetectImageIsDisabled && typeof(options.onDetectImageIsDisabled)=='function')?options.onDetectImageIsDisabled:function(){};
+	var disabledActionRun = function(){
+		if(actionCounter) return;
+		actionCounter++;
+		disabledAction();
+	}
+	/* create image */
+	var img = new Image();
+	var currentTime = (new Date()).getTime();
+	if(navigator.appName.indexOf('Microsoft Internet Explorer') != -1){// ie
+		img.onload = enaledActionRun;
+		img.onabort = enaledActionRun;
+		img.onerror = enaledActionRun;
+		img.src = currentTime+'.'+currentTime+'?time='+currentTime;
+		setTimeout(function(){
+			disabledActionRun();
+		}, 0);
+	}else if (navigator.appName.indexOf('Opera') != -1) {// opera
+		img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="+'?time='+(new Date()).getTime();
+		if(img.complete){
+			enaledActionRun();
+		}else{
+			disabledActionRun();
+		}
+	}else{// other
+		img.src = currentTime+'.'+currentTime+'?time='+currentTime;
+		if(img.complete){
+			disabledActionRun();
+		}else{
+			enaledActionRun();
+		}
+	}
+}
+// tested in: ff 2+, opera 9+, chrome, safari 4+, ie 6+
