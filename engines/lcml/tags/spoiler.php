a5c4c27b3c393a20ea423ddf61755167b3237b41
Balancer <balancer@balancer.ru>
2013-12-01 14:25:09 +0400

    [spoiler] теперь шифруется base64 для скрытия от поисковиков.

--- engines/lcml/tags/spoiler.php
+++ engines/lcml/tags/spoiler.php
@@ -3,5 +3,5 @@
 function lp_spoiler($txt,$params)
 {
 	$id = time().rand();
-	return "<a href=\"#\" class=\"spoiler\" onClick=\"var d=(document.getElementById('$id')).style; if(d.display==''){d.display='none';(document.getElementById('_$id')).innerHTML='показать'}else{d.display='';(document.getElementById('_$id')).innerHTML='скрыть'}; return false;\">".@$params['description']."&nbsp[<span id=\"_$id\">показать</span>]</a><div id=\"$id\" style=\"display: none; border: 1px dotted; margin: 0px 10px 4px 20px; padding: 2px;\">".lcml($txt)."</div>\n";
+	return "<a href=\"#\" class=\"spoiler\" onClick=\"var d=(el=(document.getElementById('$id'))).style; if(d.display==''){d.display='none';el.innerHTML=btoa(el.innerHTML);(document.getElementById('_$id')).innerHTML='показать'}else{el.innerHTML=atob(el.innerHTML);d.display='';(document.getElementById('_$id')).innerHTML='скрыть'}; return false;\">".@$params['description']."&nbsp[<span id=\"_$id\">показать</span>]</a><div id=\"$id\" style=\"display: none; border: 1px dotted; margin: 0px 10px 4px 20px; padding: 2px;\">".base64_encode(lcml($txt))."</div>\n";
 }
