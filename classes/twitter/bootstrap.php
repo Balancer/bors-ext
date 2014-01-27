dd24d2461dabd1255bfc06413bfe5adb7c18566f
Balancer <balancer@balancer.ru>
2014-01-28 03:36:33 +0400

    Последнее обновление Bootstrap2. Дальше как-то придётся мириться с 3-м :-/

--- classes/twitter/bootstrap.php
+++ classes/twitter/bootstrap.php
@@ -4,21 +4,22 @@ class twitter_bootstrap
 {
 	static function load($responsive = true)
 	{
+		$path = config('bootstrap.path');
 		// Пишется раньше, так как следующее добавление будет pre
 		if($responsive)
-			bors_use('pre:/_bors3rdp/'.config('bootstrap.path').'/css/bootstrap-responsive.min.css');
+			bors_use('pre:'.$path.'/css/bootstrap-responsive.min.css');
 
-		bors_use('pre:/_bors3rdp/'.config('bootstrap.path').'/css/bootstrap.min.css');
+		bors_use('pre:'.$path.'/css/bootstrap.min.css');
 
 		jquery::load();
-		bors_use('/_bors3rdp/'.config('bootstrap.path').'/js/bootstrap.min.js');
+		bors_use(''.$path.'/js/bootstrap.min.js');
 		config_set('css_bootstrap_is_loaded', true);
 	}
 
 	static function jquery_ui()
 	{
-		bors_use('pre:/_bors3rdp/bootstrap-plugins/'.config('bootstrap.jquery_ui').'/css/custom-theme/jquery-ui-1.8.16.custom.css');
-		bors_use('/_bors3rdp/bootstrap-plugins/'.config('bootstrap.jquery_ui').'/js/jquery-ui-1.8.16.custom.min.js');
+		bors_use('pre:/_bors-3rd/opt/bootstrap-plugins/'.config('bootstrap.jquery_ui').'/css/custom-theme/jquery-ui-1.8.16.custom.css');
+		bors_use('/_bors-3rd/opt/bootstrap-plugins/'.config('bootstrap.jquery_ui').'/js/jquery-ui-1.8.16.custom.min.js');
 	}
 
 	static function raw_message($params = array())
