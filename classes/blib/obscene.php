88e52134e3f5238707344ace73290d5134257cc1
Balancer <balancer@balancer.ru>
2014-03-18 23:30:05 +0400

    Доработки фильтра мата.

--- classes/blib/obscene.php
+++ classes/blib/obscene.php
@@ -13,30 +13,30 @@ class blib_obscene
 
 	static function mask($text, $abusive = false)
 	{
-		$text=preg_replace_callback("/(?<!(ра))([xх])([yу][йияюеeё])/ui", 'blib_obscene::stars2', $text);
-		$text=preg_replace_callback("/([xх])([yу][л][яи])(\b|[^г])/ui", 'blib_obscene::stars', $text);
-		$text=preg_replace_callback("/([^рpт][^аaи][xх])([yу])([йяюеe]|её)/ui", 'blib_obscene::stars', $text);
-		$text=preg_replace("/([^РрPpТтT][^АаAaИи])([XxХх])\.*[YyУу]\.*[йЙяЯюЮеЕeEe]\.*/u","\$1\$2***",$text);
-		$text=preg_replace("/([Пп])[Ии][Зз3][Дд]/u","\$1***",$text);
-		$text=preg_replace("/([Пп])\.*[Ии]\.*[Зз3]\.*[Дд]\.*/u","\$1***",$text);
-		$text=preg_replace("/(?<![рРpP][уУyYaAаА])([Бб6])[Лл][Яя]$/u","\$1**",$text);
-		$text=preg_replace("/([Бб6])[Лл][Яя][Дд]/u","\$1***",$text);
-		//$text=preg_replace("/([Бб])[Лл][Яя]([дД]|\s)/u","\$1**\$2",$text);
-		$text=preg_replace("/(?<![рРpP][уУyYаАaA])([Бб])[Лл][Яя]\S*(\b|[дД]|\s)/u","\$1**\$2",$text);
-//$text=preg_replace("/манд(а[^р]|а[^т]|и|е|у|ой|ы)/u","м***",$text);
-		$text=preg_replace("/^([ЕеEeЁё])[Бб][TТтAaАаиУуYy]/u","\$1***",$text);
-		$text=preg_replace("/([^рРpPлЛдДНнчЧтТTвВ])([ЕеEeЁё])[Бб]([\sTТтAaАаиИУуYy])/u","\$1\$2**\$3",$text);
-		$text=preg_replace("/(?<![СсCc][КкKk][Ии])([Пп])[Ии][Дд][AaАаOoОо]([PpРр])/u","\$1***\$2",$text);
+		$text = preg_replace_callback("/(?<!(ра))([xх])([yу][йияюеeё])/ui", 'blib_obscene::stars2', $text);
+		$text = preg_replace_callback("/([xх])([yу][л][яи])(\b|[^г])/ui", 'blib_obscene::stars', $text);
+		$text = preg_replace_callback("/([^рpт][^аaи][xх])([yу])([йяюеe]|её)/ui", 'blib_obscene::stars', $text);
+		$text = preg_replace("/([^РрPpТтT][^АаAaИи])([XxХх])\.*[YyУу]\.*[йЙяЯюЮеЕeEe]\.*/u","\$1\$2***",$text);
+		$text = preg_replace("/([Пп])[Ии][Зз3][Дд]/u","\$1***",$text);
+		$text = preg_replace("/([Пп])\.*[Ии]\.*[Зз3]\.*[Дд]\.*/u","\$1***",$text);
+		$text = preg_replace("/(?<![рРpP][уУyYaAаА])([Бб6])[Лл][Яя]$/u","\$1**",$text);
+		$text = preg_replace("/([б6])[л][я][д]/ui","\$1***",$text);
+		//$text = preg_replace("/([Бб])[Лл][Яя]([дД]|\s)/u","\$1**\$2",$text);
+		$text = preg_replace_callback("/(?<![рp][уyаaеe])([б])([л][я]\S*)(\b|[д]|\s)/ui", 'blib_obscene::stars', $text);
+//$text = preg_replace("/манд(а[^р]|а[^т]|и|е|у|ой|ы)/u","м***",$text);
+		$text = preg_replace("/^([ЕеEeЁё])[Бб][TТтAaАаиУуYy]/u","\$1***",$text);
+		$text = preg_replace("/([^рРpPлЛдДНнчЧтТTвВ])([ЕеEeЁё])[Бб]([\sTТтAaАаиИУуYy])/u","\$1\$2**\$3",$text);
+		$text = preg_replace("/(?<![СсCc][КкKk][Ии])([Пп])[Ии][Дд][AaАаOoОо]([PpРр])/u","\$1***\$2",$text);
 
-		$text=preg_replace("/(\s|при|за|у|под|на)[еEёЁeE][бБ]/u","\$1e*",$text);
+		$text = preg_replace("/(\s|при|за|у|под|на)[еEёЁeE][бБ]/u","\$1e*",$text);
 
 		if($abusive)
 		{
 			$text = preg_replace("/([г])([aаoо])[BВв][HНн]/ui", "\$1***", $text);
-			$text=preg_replace("/([СсCc])[УуYy][КкKk]([аАaAиИеЕуУyY]|ой)/u","\$1**\$2",$text);
-			$text=preg_replace("/([MmМм])[УуYy][Дд][AaАаИи]([лЛкКkK][аАaA]?)/u","\$1***\$2",$text);
-		//$text=preg_replace("/([Пп])[Ии][Дд][AaАаOoОо]([PpРр])/u","\$1***\$2",$text);
-		//$text=preg_replace("/жоп(а|и|е|у|ой)/u","ж**",$text);
+			$text = preg_replace("/([СсCc])[УуYy][КкKk]([аАaAиИеЕуУyY]|ой)/u","\$1**\$2",$text);
+			$text = preg_replace("/([MmМм])[УуYy][Дд][AaАаИи]([лЛкКkK][аАaA]?)/u","\$1***\$2",$text);
+		//$text = preg_replace("/([Пп])[Ии][Дд][AaАаOoОо]([PpРр])/u","\$1***\$2",$text);
+		//$text = preg_replace("/жоп(а|и|е|у|ой)/u","ж**",$text);
 		}
 
 		return $text;
@@ -44,7 +44,7 @@ class blib_obscene
 
 	function __dev()
 	{
-		$text = "Эти истребители — такое говно. Застрахуйте корабля от рубля. Хулиганы.\n"
+		$text = "Эти истребители — такое говно. Застрахуйте корабля от рубля. Хулиганы потребляет потреблять.\n"
 			.base64_decode('0JzQvdC+0LPQviDRgdGC0LDQu9C+INCyINC90LDRiNC4INC00L3QuCDQvdC10L7Qv9C+0LfQvdCw0L3QvdC+0LkgWNCj0JnQndCYLgo=')
 			.base64_decode('0JTQsCDRhdGD0LvQuCDRgtCw0LwsINCh0L/QtdGA0LzQsNC90LTQsNCx0LvRj9C00YHQutCw0Y8g0L/QuNC30LTQvtC/0YDQvtGR0LHQuNC90LAg0LrQsNC60LDRjy3RgtC+Lgo=')
 			.base64_decode('0K8g0YXRg9C10Y4sINC00L7RgNC+0LPQsNGPINGA0LXQtNCw0LrRhtC40Y8uCg==')
