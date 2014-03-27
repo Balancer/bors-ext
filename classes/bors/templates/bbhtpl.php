b3f1f474d0352082c47c10b8471f4ac429855662
Balancer <balancer@balancer.ru>
2014-03-28 03:12:49 +0400

    E_STRICT

--- classes/bors/templates/bbhtpl.php
+++ classes/bors/templates/bbhtpl.php
@@ -2,7 +2,7 @@
 
 class bors_templates_bbhtpl extends bors_templates_smarty3
 {
-	static function fetch($template, $data)
+	static function fetch($template, $data = array(), $instance=NULL)
 	{
 		$hts = parent::fetch($template, $data);
 		return lcml_bbh($hts);
