62da80a7b52e888cd9382e0494c82addda437d1c
Balancer <balancer@balancer.ru>
2014-06-15 12:59:01 +0400

    Добавляем импорт тупым парсингом html.

--- classes/web/twitter.php
+++ classes/web/twitter.php
@@ -26,4 +26,46 @@ class web_twitter
 
 		return $metas;
 	}
+
+	static function import_bb($url)
+	{
+		$html = blib_http::get($url);
+
+		file_put_contents('test.html', $html);
+
+		$dom= new DOMDocument('1.0', 'UTF-8');
+		@$dom->loadHTML($html);
+		$dom->encoding="UTF-8";
+		$xpath = new DOMXPath($dom);
+
+
+
+		$result = array();
+
+		// Чистим мусор
+		foreach(array(
+			"//div[contains(@class, 'replies-to')]",
+			"//span[contains(@class, 'u-isHiddenVisually')]",
+		) as $query)
+			foreach($xpath->query($query) as $node)
+				$node->parentNode->removeChild($node);
+
+		$ava = $xpath->query('//a[contains(@class,"js-user-profile-link")]')->item(0);
+		$img_src = $xpath->query('//img[@class="avatar js-action-profile-avatar"]')->item(0)->getAttribute('src');
+		$twitter_name = $xpath->query('//span[@class="username js-action-profile-name"]')->item(0)->nodeValue;
+		$full_name = $xpath->query('//strong[contains(@class, "fullname")]')->item(0)->nodeValue;
+		$text = $xpath->query('//p[contains(@class,"tweet-text")]')->item(0)->nodeValue;
+
+		$bb_code = "[round_box][img={$img_src} 100x100 left flow nohref resize][h][a href=\"{$url}\"]{$full_name} [small]{$twitter_name}[/small][/a][/h]
+[big]{$text}[/big]
+
+[span class=\"transgray\"][reference]".bors_external_feeds_entry::url_host_link($url)."[/reference][/span][/round_box]";
+
+		return $bb_code;
+	}
+
+	static function __dev()
+	{
+		echo self::import('https://twitter.com/navalny/status/477862956274057216');
+	}
 }
