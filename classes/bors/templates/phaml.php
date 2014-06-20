31987d26e4de481002586fd886f4ffeaca7c62bb
Balancer <balancer@balancer.ru>
2014-06-20 05:33:20 +0400

    А так?

--- classes/bors/templates/phaml.php
+++ classes/bors/templates/phaml.php
@@ -9,29 +9,25 @@ class bors_templates_phaml
 {
 	static function fetch($template, $data)
 	{
-		require_once config('phamlp_path').'/haml/HamlParser.php';
+//		require_once config('phamlp_path').'/haml/HamlParser.php';
 
-		$haml = new HamlParser(array('style'=>'nested'));
+//		$haml = new HamlParser(array('style'=>'nested'));
+		$haml = new MtHaml\Environment('php');
 		$template = str_replace('xfile:', '', $template);
 //		echo "\n-------------\n$template:\n".file_get_contents($template)."\n---------------\n\n";
 
-		$php = $haml->parse($template);
-
-		// Результат парсинга — php-контент. Сохраним его и заинклудим
-
-		$tmp = tempnam(config('cache_dir'), 'phamlp-');
-		file_put_contents($tmp, $php);
+		$executor = new MtHaml\Support\Php\Executor($haml, array(
+			'cache' => config('cache_dir'). '/mthaml',
+		));
 
 		ob_start();
-		$err_rep_save = error_reporting();
-		error_reporting($err_rep_save & ~E_NOTICE);
-		extract($data);
-		require($tmp);
-		error_reporting($err_rep_save);
+
+		$executor->display($template, array(
+			'var' => 'value',
+		));
+
 		$html = ob_get_contents();
 		ob_end_clean();
-
-		unlink($tmp);
 
 		return $html;
 	}
