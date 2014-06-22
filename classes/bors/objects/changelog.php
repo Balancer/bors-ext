a8de603e8925f3ff157495f650f02befedb6a653
Balancer <balancer@balancer.ru>
2014-06-22 17:45:31 +0400

    Комментарии к измеениям при логгировании.

--- classes/bors/objects/changelog.php
+++ classes/bors/objects/changelog.php
@@ -13,6 +13,7 @@ class bors_objects_changelog extends bors_object_db
 			'old_value',
 			'target_class_name',
 			'target_id',
+			'comment' => array('edit_type' => 'bbcode'),
 			'last_editor_id',
 			'last_editor_ip',
 			'last_editor_ua',
@@ -26,11 +27,14 @@ class bors_objects_changelog extends bors_object_db
 			if(in_array($property_name, array('last_editor_ua', 'last_editor_ip', 'last_editor_id', 'modify_time', 'object_id', 'form_class_name')))
 				continue;
 
+var_dump($object->data); exit();
+
 			bors_new('bors_objects_changelog', array(
 				'property_name' => $property_name,
 				'old_value' => $old_value,
 				'target_class_name' => $object->class_name(),
 				'target_id' => $object->id(),
+				'comment' => $object->get('bors_changes_comment'),
 			));
 		}
 	}
