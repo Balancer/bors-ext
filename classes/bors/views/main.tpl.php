4d2e69df8fdbce51321939f151fe3850af42c76a
Balancer <balancer@balancer.ru>
2014-10-01 06:32:22 +0400

    Переходим на работу с layouts.

--- classes/bors/views/main.tpl.php
+++ classes/bors/views/main.tpl.php
@@ -1,7 +1,7 @@
 <?php
 
-echo $self->pages_links_nul();
+echo $self->layout()->mod('pagination');
 
 bors_module::show_mod('bors_pages_module_paginated_items', array('items' => $items, 'class' => $self->main_class()));
 
-echo $self->pages_links_nul();
+echo $self->layout()->mod('pagination');
