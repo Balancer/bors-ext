bf8504ca7c28c36cb5561affea0afba437dedff4
Balancer <balancer@balancer.ru>
2014-12-27 12:04:08 +0300

    ++

--- classes/bors/util/dump.php
+++ classes/bors/util/dump.php
@@ -4,13 +4,27 @@ class bors_util_dump
 {
 	static function run($argv)
 	{
-		$class_name = @$argv[0];
-		$object_id = @$argv[1];
-
-		$x = bors_load($class_name, $object_id);
-		if(!$x)
-			echo "Can't load $class_name($object_id)\n";
+		if(preg_match('!http://!', @$argv[0]))
+		{
+			$x = bors_load_uri($argv[0]);
+			if(!$x)
+			{
+				echo "Can't load {$argv[0]})\n";
+				return;
+			}
+		}
 		else
-			echo json_encode($x->data, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+		{
+			$class_name = @$argv[0];
+			$object_id = @$argv[1];
+			$x = bors_load($class_name, $object_id);
+			if(!$x)
+			{
+				echo "Can't load $class_name($object_id)\n";
+				return;
+			}
+		}
+
+		echo json_encode($x->data, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 	}
 }
