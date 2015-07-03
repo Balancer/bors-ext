7989b219f25da4085e655f1e37805615ac019b60
Balancer <balancer@balancer.ru>
2015-07-04 01:37:12 +0300

    Больше возможностей у codemirror.

--- classes/assets/codemirror.php
+++ classes/assets/codemirror.php
@@ -7,7 +7,7 @@ class assets_codemirror extends bors_asset
 	// http://stackoverflow.com/questions/16804771/codemirror-3-0-format-preloaded-textarea-code
 	// http://stackoverflow.com/questions/9374894/can-codemirror-find-textareas-by-class
 
-	static function load($modes, $els)
+	static function load($modes, $els, $params)
 	{
 		$path = config('codemirror.path');
 		bors_use($path.'/lib/codemirror.js');
@@ -15,14 +15,21 @@ class assets_codemirror extends bors_asset
 		foreach(explode(',', $modes) as $mode)
 			bors_use($path.'/mode/'.$mode.'/'.$mode.'.js');
 
+		$height = popval($params, 'height');
+		$mode   = popval($params, 'mode', 'text/html');
+		if($theme	= popval($params, 'theme'))
+			bors_use($path.'/theme/'.$theme.'.css');
+
 		jquery::on_ready("
 			$({$els}).each(function(idx){
 				$(this).attr('id', 'codemirror-'+idx)
 				CodeMirror.fromTextArea(document.getElementById('codemirror-' + idx), {
-					mode: 'text/html',
-					lineNumbers: true,
+					mode: '$mode',
+					lineNumbers: true"
+.($height ? ",\n\t\t\t\t\theight: $height" : '')
+.($theme  ? ",\n\t\t\t\t\ttheme: '$theme'" : '')
+."
 //					lineWrapping: true,
-//					theme: 'monokai',
 				})
 			})
 		");
