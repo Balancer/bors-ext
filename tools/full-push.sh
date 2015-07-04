fff7b8c36bd1a36822edf5cfc3f1ab0183756085
Balancer <balancer@balancer.ru>
2015-07-05 00:25:27 +0300

    ++

--- tools/full-push.sh
+++ tools/full-push.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-for dir in *; do
-	if [[ -e $dir/.hg/hgrc ]]; then
-		echo -e "\e[1;37m=== $dir ===\e[0m"
-		echo -ne "\033]0;hg push $dir \007"
-		cd $dir
+for REPO in *; do
+	if [[ -e $REPO/.hg/hgrc ]]; then
+		echo -e "\e[1;37m=== $REPO ===\e[0m"
+		echo -ne "\033]0;hg push $REPO \007"
+		cd $REPO
 		hg ci
 		hg push
+		cd ..
+	fi
+
+	if [[ -e $REPO/.git/config ]]; then
+		echo -e "\e[1;37m=== $REPO ===\e[0m"
+		echo -ne "\033]0;git push $REPO \007"
+		cd $REPO
+		git push
 		cd ..
 	fi
 done
