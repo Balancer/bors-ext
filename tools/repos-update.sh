ac5bc325683108798a21207f4adf662fb6edf020
Balancer <balancer@balancer.ru>
2015-07-12 04:23:57 +0300

    Console tools improvements.

--- tools/repos-update.sh
+++ tools/repos-update.sh
@@ -4,26 +4,26 @@ clear
 
 for REPO in *; do
 	if [[ -e $REPO/.hg/hgrc ]]; then
-		echo -e "\e[1;37m=== $REPO ===\e[0m"
-		echo -ne "\033]0;hg push $REPO \007"
+		echo -e "\e[1;30m=== $REPO [hg]  ===\e[0m"
+		# echo -ne "\033]0;hg push $REPO \007"
 		cd $REPO
-		hg pull
-		hg up
+		hg -q pull | prerror.sh Ошибка $REPO hg pull
+		hg -q up | prerror.sh Ошибка $REPO hg up
 		cd ..
 	fi
 	if [[ -e $REPO/.git/config ]]; then
-		echo -e "\e[1;37m=== $REPO ===\e[0m"
-		echo -ne "\033]0;git pull $REPO \007"
+		echo -e "\e[1;30m=== $REPO [git] ===\e[0m"
+		# echo -ne "\033]0;git pull $REPO \007"
 		cd $REPO
-		git pull
+		git pull -q 2>&1 | prerror.sh Ошибка $REPO git pull
 		cd ..
 	fi
 done
 
 for REPO in *.git; do
-	echo -e "\e[1;37m=== $REPO ===\e[0m"
-	echo -ne "\033]0;git fetch $REPO \007"
+	echo -e "\e[1;30m=== $REPO [git bare] ===\e[0m"
+	# echo -ne "\033]0;git fetch $REPO \007"
 	cd $REPO
-	git fetch
+	git fetch -q 2>&1 | prerror.sh Ошибка $REPO git fetch
 	cd ..
 done
