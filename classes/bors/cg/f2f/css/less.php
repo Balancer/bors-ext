f3e462bf12ea21a93b8a3944598878ba4b07d186
Balancer <balancer@balancer.ru>
2015-07-31 06:49:30 +0300

    better less 2 css.

--- classes/bors/cg/f2f/css/less.php
+++ classes/bors/cg/f2f/css/less.php
@@ -8,11 +8,24 @@ class bors_cg_f2f_css_less extends bors_cg_f2f
 
 	function content()
 	{
-		$src = $_SERVER['DOCUMENT_ROOT'].'/_cg/f2f/'.$this->id().'.less';
-		$dst = $_SERVER['DOCUMENT_ROOT'].'/_cg/f2f/'.$this->id().'.less.css';
+		$relative_path = '/f2f/'.$this->id().'.less';
+		$src = $_SERVER['DOCUMENT_ROOT'].'/_cg'.$relative_path;
+//		$dst = $_SERVER['DOCUMENT_ROOT'].'/_cg'.$relative_path.'.css';
+
+		if(!file_exists($src))
+			$src = bors::find_webroot($relative_path);
+
+		if(!file_exists($src))
+			throw new Exception("Can't find ".$relative_path);
+
+//		$cache = bors_cache_fast('f2f_css_less', $src);
+//		if(filemtime($src) <= )
+
 		$less = new lessc;
-//		$less->checkedCompile($src, $dst);
-		return $less->compileFile($src);
-//		return file_get_contents($dst);
+		$css = $less->compileFile($src);
+
+//		file_put_contents($dst, $css);
+
+		return $css;
 	}
 }
