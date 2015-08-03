a685fedaf7d7d955fd63d98ef4bc2d8c49f945f7
Balancer <balancer@balancer.ru>
2015-08-03 05:57:12 +0300

    less-css works.

--- classes/bors/cg/f2f/css/less.php
+++ classes/bors/cg/f2f/css/less.php
@@ -8,7 +8,15 @@ class bors_cg_f2f_css_less extends bors_cg_f2f
 
 	function content()
 	{
-		$src = $_SERVER['DOCUMENT_ROOT'].'/_cg/f2f/'.$this->id().'.less';
+		$relative_path = '/f2f/'.$this->id().'.less';
+		$src = $_SERVER['DOCUMENT_ROOT'].'/_cg'.$relative_path;
+
+		if(!file_exists($src))
+			$src = bors::find_webroot($relative_path);
+
+		if(!file_exists($src))
+			throw new Exception("Can't find ".$relative_path);
+
 		$dst = $_SERVER['DOCUMENT_ROOT'].'/_cg/f2f/'.$this->id().'.less.css';
 		$less = new lessc;
 //		$less->checkedCompile($src, $dst);
